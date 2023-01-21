729088ab8e44be59a0deefc46edc0015e6f7b4ca
pablomoga9 <moyagarcia99@gmail.com>
2023-01-21 21:34:52 +0100

    add/login,register,logout routes & request functions

---
+++ front-react/src/components/DefaultLayout.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link, Navigate, Outlet } from 'react-router-dom'
+import { useStateContext } from '../contexts/ContextProvider';
+
+function DefaultLayout() {
+    const {user,token} = useStateContext()
+
+    if(!token){
+        return <Navigate to="/login"/>
+    }
+
+    const onLogout = (ev)=>{    
+        ev.preventDefault();
+    }
+
+  return (
+    <div id='defaultLayout'>
+        <aside>
+            <Link to="/dashboard">Dashboard</Link>
+            <Link to="/users">Users</Link>
+        </aside>
+        <div className='content'>
+            <header>
+                <div>
+                    Header
+                </div>
+                <div>
+                    {user.name}
+                    <a href="#" onClick={onLogout} className='btn-logout'></a>
+                </div>
+            </header>
+            <main>
+                <Outlet/>
+            </main>
+        </div>
+        <Outlet/>    
+    </div>
+  )
+}
+
+export default DefaultLayout
